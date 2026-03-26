5ae36d72e439c0e3aa075f068da7238038036eb8
Kinmiri <83705766+Kinmiri@users.noreply.github.com>
2026-03-26 19:44:52 +0300

    1.py

--- 1.py
+++ 1.py
@@ -4,3 +4,11 @@ def decrypt_AES(bvp, tzgyvikvok, krx, efetv):
     cipher = AES.new(bvp, AES.MODE_GCM, efetv)
     plaintext = cipher.decrypt_and_verify(tzgyvikvok, krx)
     return plaintext
+    
+bvp = b':)'
+tzgyvikvok = b':)'
+krx = b':)'
+efetv = b':)'
+
+decrypted_text = decrypt_AES(bvp, tzgyvikvok, krx, efetv)
+print(decrypted_text)
